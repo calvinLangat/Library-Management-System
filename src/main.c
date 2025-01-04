275cb93ed46a659024617b23855c0bc7cf2d259c
Calvin Langat <calvokkt@gmail.com>
2025-01-04 15:16:44 +0000

    Adding to DB, searching and deleting works

--- src/main.c
+++ src/main.c
@@ -1,14 +1,15 @@
 #include "database.h"
 
-
 int main(int argc, char* argv[])
 {
 	booksInLibrary = 0;
 	slotsUsed = 0;
 
+
+	char* file = "..\\testdata\\books.csv";
 	// Use array to store the book structs
-	book_t* books = malloc(MAXBOOKS*sizeof(book_t));
-	if(books == NULL)
+	book_t* booksArray = malloc(MAXBOOKS*sizeof(book_t));
+	if(booksArray == NULL)
 	{
 		printf("Failed to heap allocate memory for book array.\n");
 		return EXIT_FAILURE;
@@ -20,23 +21,47 @@ int main(int argc, char* argv[])
 	book_t book = {0};
 
 	// Just a placeholder
-	if(!insertBook(books, &booktree, &book))
-	{
-		printf("Failed to insert book.\n");
-	}
+	// if(!insertBook(booksArray, &booktree, &book))
+	// {
+	// 	printf("Failed to insert book.\n");
+	// }
 
+
+	if(!insertBooksFromCSV(file, &booktree, booksArray))
+	{
+		printf("Error inserting book to Database.\n");
+	}
+	printf("books in lib %d\n", booksInLibrary);
 	book_t book1;
 	
 	// Another placeholder
-	if(!searchBook(booktree, books, "", 1, &book1))
+	if(!searchBook(booktree, booksArray, "6512177277062", 1, &book1))
 	{
-		printf("Could not find book.\n");
+		printf("Could not find book title: %s.\n", book1.title);
+	}
+	else
+	{
+		printf("Found book title: %s.\n", book1.title);
 	}
 
-	if(!deleteBook(&booktree, books, "", 1))
+	if(!deleteBook(&booktree, booksArray, "6512177277062", 1))
 	{
 		printf("Failed to delete book.\n");
 	}
+	else
+	{
+		printf("Delete successful.\n");
+	}
+
+	if(!searchBook(booktree, booksArray, "6512177277062", 1, &book1))
+	{
+		printf("Could not find book title: %s.\n", book1.title);
+	}
+	else
+	{
+		printf("Found book title: %s.\n", book1.title);
+	}
+
 
 	return 0;
 }
